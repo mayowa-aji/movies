cc4904c4cc99065c3eccf665d9b6c94fbf94750a
Mayowa Ajiboye <64916584+mayowa-aji@users.noreply.github.com>
2023-03-02 10:23:07 -0500

    Complete code challenge

--- src/flatflix.py
+++ src/flatflix.py
@@ -1,52 +1,75 @@
 class Movie:
     def __init__(self, title):
-        pass
+        self.title = title
+        self.reviews = []
 
     def get_title(self):
-        pass
+        return self.title
 
     def get_reviews(self):
-        pass
+        return self.reviews
 
     def get_viewers(self):
-        pass
+        result = []
+        for review in self.reviews:
+            result.append(review.viewer)
+        return result
 
     def average_rating(self):
-        pass
+        total = 0
+        for review in self.reviews:
+            total += review.rating
+        return total/len(self.reviews)
 
-    def highest_rating(self):
-        pass
+    def highest_review(self):
+        highest = self.reviews[0]
+        for review in self.reviews:
+            if review.rating > highest.rating:
+                highest = review
+        return highest
+
 
 
 class Viewer:
     def __init__(self, username):
-        pass
+        self.username = username
+        self.reviews = []
 
     def get_username(self):
-        pass
+        return self.username
 
     def get_reviews(self):
-        pass
+        return self.reviews
 
     def get_movies(self):
-        pass
+        result = []
+        for review in self.reviews:
+            result.append(review.movie)
+        return result
 
     def has_reviewed(self, movie):
-        pass
+        for review in self.reviews:
+            if(review.movie == movie):
+                return True
+            else:
+                return False
 
     def rate_movie(self, movie, rating):
-        pass
+        new_review = Review(self, movie, rating)
+        self.reviews.append(new_review)
 
 
 class Review:
     def __init__(self, viewer, movie, rating):
-        pass
+        self.viewer = viewer
+        self.movie = movie
+        self.rating = rating
 
     def get_rating(self):
-        pass
+        return self.rating
 
     def get_viewer(self):
-        pass
+        return self.viewer
 
     def get_movie(self):
-        pass
+        return self.movie
